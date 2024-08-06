ac63b6c994a9762db5165cbba5c28f34186e4499
kriti <kriti.jindal.37@gmail.com>
2024-08-07 03:31:55 +0530

    adding structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-
+import Sidebar from "./components/Sidebar/Sidebar"
 function App() {
-  const [count, setCount] = useState(0)
   
-
   return (
     <>
-      <h1>hello everyone</h1>
+      <Sidebar/>
+      
     </>
   )
 }
