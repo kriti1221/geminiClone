ada76283f48c6ca235465ffc0f6cf87c44de371e
kriti <kriti.jindal.37@gmail.com>
2024-08-07 08:02:59 +0530

    main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar/Sidebar"
+import Main from "./components/Main/Main"
 function App() {
   
   return (
     <>
       <Sidebar/>
-      
+      <Main/>
+
     </>
   )
 }
