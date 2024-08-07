64d958b41cb64a70a354610d6267cd02a9bda97e
kriti <kriti.jindal.37@gmail.com>
2024-08-07 09:14:25 +0530

    securing Api Key

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -19,17 +19,15 @@ const {
     responseMimeType: "text/plain",
   };
   
-  async function run() {
+  async function run(prompt) {
     const chatSession = model.startChat({
       generationConfig,
-   // safetySettings: Adjust safety settings
-   // See https://ai.google.dev/gemini-api/docs/safety-settings
       history: [
       ],
     });
   
-    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+    const result = await chatSession.sendMessage(prompt);
     console.log(result.response.text());
   }
   
-  run();
+  export default run;
